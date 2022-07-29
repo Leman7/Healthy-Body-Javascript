107fe9eb11a96b3d3f7d4dc4a3d3c0e39fe56acd
Leman7 <mmehrwaldt@gmail.com>
2022-07-29 17:22:50 -0300

    cambios menores

--- js/baseDatos.js
+++ js/baseDatos.js
@@ -10,7 +10,3 @@ export const productos = [{img: '1', tipo: 'Proteina', cantidad: 1, precio: 1000
                           {img: '8', tipo: 'Bolso', cantidad: 1, precio: 1000},
                           {img: '9', tipo: 'Shaker', cantidad: 1, precio: 1000}]
 
-
-// export const fotos = ["../assets/proteina.png", "../assets/vitamina.png", "../assets/creatina.png", 
-//                       "../assets/testosterona.png", "../assets/turbogel.png", "../assets/combo.png",
-//                       "../assets/bolso.png", "../assets/shaker.png"]
